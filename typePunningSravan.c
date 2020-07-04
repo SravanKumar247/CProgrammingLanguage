3e8329248d6b5d2c3a0acb8b7d1341a541d44273
SravanKumar247 <54015944+SravanKumar247@users.noreply.github.com>
2020-07-04 12:06:48 +0530

    Update typePunningSravan.c

--- typePunningSravan.c
+++ typePunningSravan.c
@@ -1,1 +1,15 @@
-
+#include<stdio.h>
+int main()
+{
+  union tyepun
+  {
+     int intval;
+    char chrval[2];
+  };
+  union values val;
+  val.chrval[0]='A',val.chrval[1]='B';
+  printf("%c %c %d\n",val.chrval[0],val.chrval[1],val.intval);
+  char c[2]={'C','D'};
+  int *y=(int *)&c;
+  printf("%c %c %d\n",c[0],c[1],*y);
+}
